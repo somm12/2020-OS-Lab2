cf0f2ee1b9b519cfc1bd3cefe24eb584cab82df0
museongseo <to1376@naver.com>
2020-05-03 00:52:22 +0900

    Merge both of code

--- lab2_bst.c
+++ lab2_bst.c
@@ -1,8 +1,10 @@
 /*
  *	Operating System Lab
  *	    Lab2 (Synchronization)
- *	    Student id : 
- *	    Student name : 
+ *	    Studend id : 32182490
+ *	    Student name : An Somin
+ *	    Student id : 32182110
+ *	    Student name : Seo Museong
  *
  *   lab2_bst.c :
  *       - thread-safe bst code.
@@ -21,75 +23,75 @@
 #include "lab2_sync_types.h"
 pthread_mutex_t lock;
 
-void lab2_node_inorder(lab2_tree *tree, lab2_node *cur) { // inorder function
-	// You need to implement lab2_node_print_inorder function.
+
+void lab2_node_inorder(lab2_tree *tree, lab2_node *cur) { // 중위순회 구현 : 왼쪽 하위 트리 -> 루트 -> 오른쪽 하위트리 순서로 이동
 	if (cur != NULL) {
 		lab2_node_inorder(tree, cur->left);
 		lab2_node_inorder(tree, cur->right);
 	}
 }
 
-int lab2_node_print_inorder(lab2_tree *tree) { //inorder print function
+int lab2_node_print_inorder(lab2_tree *tree) { // lab2_node_inorder 함수와 나눠서 구현
 	lab2_node_inorder(tree, tree->root);
-	return LAB2_SUCCESS; // return success value
+	return LAB2_SUCCESS;
 }
 
-lab2_tree *lab2_tree_create() {
-	// You need to implement lab2_tree_create function.
+
+lab2_tree *lab2_tree_create() {// 트리생성 함수
+
 	lab2_tree *tree = (lab2_tree *)malloc(sizeof(lab2_tree));
 	tree->root = NULL;
 	return tree;	
 }
 
-lab2_node * lab2_node_create(int key) {
-	// You need to implement lab2_node_create function.
-	lab2_node *node = (lab2_node *)malloc(sizeof(lab2_node));
+
+lab2_node * lab2_node_create(int key) {// key값을 인자로 가지는  노드생성함수
+		lab2_node *node = (lab2_node *)malloc(sizeof(lab2_node));
 	node->left = NULL;
 	node->right = NULL;
 	node->key = key;
 	return node;
 }
 
-_Bool search_key(lab2_tree* tree, int key){
+_Bool search_key(lab2_tree* tree, int key){//트리에 해당 key값이 존재하는지 확인하기 위한 함수 
 	lab2_node* cur = tree->root;
-	while(1){
-		if(cur->key == key){
+	while(1){                //cur 포인터를 이용해서 해당key값과 비교하면서 while문으로 오른쪽 또는 왼쪽 서브트리로 이동
+		if(cur->key == key){ // key값을 찾았을 때 종료
 			return true;
 		}
-		else if(cur->key < key){
-			if (cur->right == NULL)
+		else if(cur->key < key){// key값이 더 클때 오른쪽으로 이동
+			if (cur->right == NULL)// NULL이 나올 때 까지 key값이 없다면 false로 종료
 				return false;
 			cur = cur->right;
 		}
 		else{
-			if (cur->left == NULL)
+			if (cur->left == NULL) //key값이 더 작을 때 왼쪽으로 이동 후 NULL이 나올 때까지 key값이 없다면 false로 종료
 				return false;
 			cur = cur->left;
 		}
 	}
 }
 
-int lab2_node_insert(lab2_tree *tree, lab2_node *new_node){
-	// You need to implement lab2_node_insert function.
-	if (tree->root == NULL) {
+int lab2_node_insert(lab2_tree *tree, lab2_node *new_node){ // 트리에 노드를 추가하는 함수	
+	if (tree->root == NULL) {// 루트 NULL값일 때 해당자리에 추가
 		tree->root = new_node;
+		return LAB2_SUCCESS;
+	}
+	if (search_key(tree,new_node->key)) {// 해당 key값이 이미 존재할 때  error 반환
 		return LAB2_ERROR;
 	}
-	if (search_key(tree,new_node->key)) {
-		return LAB2_ERROR;
-	}
-	lab2_node* cur = tree->root;
+	lab2_node* cur = tree->root;       // 노드를 추가할 위치를 가리키는 포인터 cur 선언
 	while (1) {
-		if (cur->key < new_node->key) {
+		if (cur->key < new_node->key) {// key값이 해당 노드 보다 크다면  cur->right이 NULL 이 될 때까지 오른쪽으로 이동
 			if (cur->right == NULL) {
-				cur->right = new_node;
+				cur->right = new_node; // 노드추가
 				return LAB2_SUCCESS;
 			}
 			cur = cur->right;
 		}
-		else {
+		else {						  // key값이 해당 노드 보다 작다면 cur->left가 NULL 이 될 때까지 왼쪽으로 이동
 			if (cur->left == NULL) {
-				cur->left = new_node;
+				cur->left = new_node;// 노드추가
 				return LAB2_SUCCESS;
 			}
 			cur = cur->left;
@@ -184,7 +186,7 @@ int lab2_node_remove(lab2_tree *tree, int key) {
 		else{ // 작다면, 왼쪽으로 이동
 			par_parent = parent;
 			parent = cur;
-			cur = cur->left;결
+			cur = cur->left;
 		}
 	}
 
@@ -376,14 +378,12 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
 	return LAB2_SUCCESS;
 }
 
-int lab2_tree_delete(lab2_tree *tree) {
-	// You need to implement lab2_tree_delete function.
+int lab2_tree_delete(lab2_tree *tree) {// 트리 삭제함수
 	free(tree);
-	return LAB2_SUCCESS; // success
+	return LAB2_SUCCESS;
 }
 
-int lab2_node_delete(lab2_node *node) {
-	// You need to implement lab2_node_delete function.
+int lab2_node_delete(lab2_node *node) {// 노드 삭제함수
 	free(node);
 	return LAB2_SUCCESS;
 }
